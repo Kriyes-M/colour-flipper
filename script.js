27372b1847dfc83960115bb1e1b6bef1f4324332
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-01 13:22:08 +0000

    Add DOM access to elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName('body')[0];
+const colorHex = document.getElementById('colorHex');
+const flipButton = document.getElementById('flipButton');
+
+console.log(body);
+console.log(colorHex);
+console.log(flipButton);
