94f95153164d55a41f21b35eae5a8e1d81c1886e
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-01 22:22:02 +0000

    Add function to generate a random hex value

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const body = document.getElementsByTagName('body')[0];
 const colorHex = document.getElementById('colorHex');
 const flipButton = document.getElementById('flipButton');
 
-console.log(body);
-console.log(colorHex);
-console.log(flipButton);
+const hexChars = '0123456789ABCDEF';
+
+function generateHex() {
+  let hexValue = '#';
+
+  for (let i = 0; i < 6; ++i) {
+    const randomNum = Math.floor(Math.random() * 16);
+    hexValue += hexChars[randomNum];
+  }
+
+  return hexValue;
+}
